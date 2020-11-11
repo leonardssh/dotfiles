91953d33283c63f251993783e05495102786c3bc
LeonardSSH <leonardssh22@gmail.com>
2020-11-11 14:40:24 +0100

    feat(zsh): add random_bg alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -23,3 +23,4 @@ alias dev='ranger ~/Development'
 alias zrc='nvim ~/.zshrc'
 alias kill_discord="ps -ef | grep discord-canary | grep -v grep | awk '{print $2}' | xargs -r kill -9"
 alias reload_polybar="polybar-msg cmd restart"
+alias random_bg="feh --bg-fill --randomize ~/Wallpapers/*"
